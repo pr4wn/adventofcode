9200c951b6e7adedeaf3ba06554b7496fc8ea700
Seán Doherty <sean.prawn@gmail.com>
2022-12-01 11:46:46 +0000

    perl tidy

--- 01/aoc-2022-1.pl
+++ 01/aoc-2022-1.pl
@@ -5,25 +5,26 @@ use warnings;
 my $fn = "./input.txt";
 
 open my $fh, '<', $fn or die $!;
-chomp (my @lines = <$fh>);
+chomp( my @lines = <$fh> );
 close($fh);
 my @elves = 0;
 my $s;
-my $elfno = 0;
+my $elfno    = 0;
 my $maxelfno = 0;
-my $maxelf = 0;
+my $maxelf   = 0;
 
 foreach my $s (@lines) {
-  if ($s eq "") {
-	if ($maxelfno <= $elves[$elfno]) {
-		$maxelf = $elfno;
-		$maxelfno = $elves[$elfno];
-	}
-	$s = "";
-    	$elfno++;
-  } else {
-      $elves[$elfno] += $s;
-  }
+    if ( $s eq "" ) {
+        if ( $maxelfno <= $elves[$elfno] ) {
+            $maxelf   = $elfno;
+            $maxelfno = $elves[$elfno];
+        }
+        $s = "";
+        $elfno++;
+    }
+    else {
+        $elves[$elfno] += $s;
+    }
 }
 
 print "Elf number $maxelf has consumed " . $maxelfno . " calories\n";
--- 01/aoc-2022-2.pl
+++ 01/aoc-2022-2.pl
@@ -5,28 +5,29 @@ use warnings;
 my $fn = "./input.txt";
 
 open my $fh, '<', $fn or die $!;
-chomp (my @lines = <$fh>);
+chomp( my @lines = <$fh> );
 close($fh);
 my @elves = 0;
 my $s;
-my $elfno = 0;
+my $elfno    = 0;
 my $maxelfno = 0;
-my $maxelf = 0;
+my $maxelf   = 0;
 my @totals;
 
 foreach my $s (@lines) {
-  if ($s eq "") {
-	push @totals, $elves[$elfno];
-	$s = "";
-    	$elfno++;
-  } else {
-      $elves[$elfno] += $s;
-  }
+    if ( $s eq "" ) {
+        push @totals, $elves[$elfno];
+        $s = "";
+        $elfno++;
+    }
+    else {
+        $elves[$elfno] += $s;
+    }
 }
 
 @totals = sort { $a <=> $b } @totals;
 my @stotals = reverse(@totals);
 
-my $j =  $stotals[0] + $stotals[1] + $stotals[2] ;
+my $j = $stotals[0] + $stotals[1] + $stotals[2];
 
 print "Top three elves have consumed " . $j . " calories\n";
