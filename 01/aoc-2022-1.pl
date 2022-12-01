5e7762005f2e82f45f367034719b13daf1ecd0cc
Seán Doherty <sean.prawn@gmail.com>
2022-12-01 10:43:51 +0000

    initial version

---
+++ 01/aoc-2022-1.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $fn = "./input.txt";
+
+open my $fh, '<', $fn or die $!;
+chomp (my @lines = <$fh>);
+close($fh);
+my @elves = 0;
+my $s;
+my $elfno = 0;
+my $maxelfno = 0;
+my $maxelf = 0;
+
+foreach my $s (@lines) {
+  if ($s eq "") {
+	if ($maxelfno <= $elves[$elfno]) {
+		$maxelf = $elfno;
+		$maxelfno = $elves[$elfno];
+	}
+	$s = "";
+    	$elfno++;
+  } else {
+      $elves[$elfno] += $s;
+  }
+}
+
+print "Elf number $maxelf has consumed " . $maxelfno . " calories\n";
