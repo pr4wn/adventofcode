01cf9f5e1ebabda7be6ad038ad8b1274caa25d8c
Seán Doherty <sean.prawn@gmail.com>
2022-12-01 11:14:25 +0000

    initial version

---
+++ 01/aoc-2022-2.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $fn = "./input.txt";
+
+open my $fh, '<', $fn or die $!;
+chomp (my @lines = <$fh>);
+close($fh);
+my @elves = 0;
+my $s;
+my $elfno = 0;
+my $maxelfno = 0;
+my $maxelf = 0;
+my @totals;
+
+foreach my $s (@lines) {
+  if ($s eq "") {
+	if ($maxelfno <= $elves[$elfno]) {
+		$maxelf = $elfno;
+		$maxelfno = $elves[$elfno];
+	}
+	push @totals, $elves[$elfno];
+	$s = "";
+    	$elfno++;
+  } else {
+      $elves[$elfno] += $s;
+  }
+}
+
+@totals = sort { $a <=> $b } @totals;
+my @stotals = reverse(@totals);
+
+my $j =  $stotals[0] + $stotals[1] + $stotals[2] ;
+
+print "Top three elves have consumed " . $j . " calories\n";
