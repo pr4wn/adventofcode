b088a34d5a90d516a3371ab448ed59de3b8097df
Seán Doherty <sean.prawn@gmail.com>
2022-12-02 06:38:46 +0000

    initial version day 2

---
+++ 02/aoc-2022-02-2.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature qw(switch);
+
+my $fn = "./input-02.txt";
+
+open my $fh, '<', $fn or die $!;
+chomp( my @lines = <$fh> );
+close($fh);
+my $score;
+my %column_0 = qw( A ROCK B PAPER C SCISSORS);
+my %column_1 = qw( X ROCK Y PAPER Z SCISSORS);
+foreach my $s (@lines) {
+    my @a = split(' ', $s);
+    if ($column_0{$a[0]} eq "ROCK") {
+      if ($column_1{$a[1]} =~ m/ROCK/) {$score += 3};
+      if ($column_1{$a[1]} =~ m/PAPER/) {$score += 4};
+      if ($column_1{$a[1]} =~ m/SCISSORS/) {$score += 8}; 
+    } elsif ($column_0{$a[0]} eq "PAPER") {
+      if ($column_1{$a[1]} =~ m/ROCK/) {$score += 1};
+      if ($column_1{$a[1]} =~ m/PAPER/) {$score += 5};
+      if ($column_1{$a[1]} =~ m/SCISSORS/) {$score += 9};    
+    } elsif($column_0{$a[0]} eq "SCISSORS") {
+      if ($column_1{$a[1]} =~ m/ROCK/) {$score += 2};
+      if ($column_1{$a[1]} =~ m/PAPER/) {$score += 6};
+      if ($column_1{$a[1]} =~ m/SCISSORS/) {$score += 7};      
+    }
+}
+print "total score: " . $score . "\n";
