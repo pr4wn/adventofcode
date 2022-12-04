ef58efb505ca096b3c6a19b6b9cd6e4d7e40e804
Seán Doherty <sean.prawn@gmail.com>
2022-12-04 07:21:07 +0000

    Trying not to use external libraries

--- 04/aoc-2022-04-2.pl
+++ 04/aoc-2022-04-2.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use List::Util qw(min max);
 
 my $fn = "./input-04.txt";
 
@@ -13,7 +12,7 @@ foreach my $s (@lines) {
     my @a  = split( ',', $s );
     my @p1 = split( '-', $a[0] );
     my @p2 = split( '-', $a[1] );
-    if ( min( $p1[1], $p2[1] ) - max( $p1[0], $p2[0] ) >= 0 ) {
+    if ( $p2[0] <= $p1[1] && $p1[0] <= $p2[1] ) {
         $score++;
     }
 }
