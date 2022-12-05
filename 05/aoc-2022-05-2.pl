107e542109bcadcf39208258db19cf8de4d32c66
Seán Doherty <sean.prawn@gmail.com>
2022-12-05 07:20:45 +0000

    initial version day 05

---
+++ 05/aoc-2022-05-2.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $fn = "./input-05.txt";
+my @stacks =
+  qw(QGPRLCTF JSFRWHQN QMPWHBF FDTSV ZFVWDLQ SLCZ FDVMBZ BJT HPSLGBNQ);
+
+open my $fh, '<', $fn or die $!;
+chomp( my @lines = <$fh> );
+close($fh);
+
+foreach my $s (@lines) {
+    if ( $s =~ /move/ ) {
+        my @a  = split ' ', $s;
+        my $m  = $a[1];
+        my $f  = $a[3];
+        my $t  = $a[5];
+        my $ch = substr( $stacks[ $f - 1 ], 0, $m );
+        $stacks[ $t - 1 ] = $ch . $stacks[ $t - 1 ];
+        for ( my $i = 1 ; $i <= $m ; $i++ ) {
+            $stacks[ $f - 1 ] =~ s/^.//;
+        }
+    }
+}
+foreach my $s (@stacks) {
+    print substr( $s, 0, 1 );
+}
+print "\n";
