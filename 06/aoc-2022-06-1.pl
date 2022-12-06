33d4257027af7900f3add3b61d2d210cf31bb3d8
Seán Doherty <sean.prawn@gmail.com>
2022-12-06 07:04:12 +0000

    initial version day 06

---
+++ 06/aoc-2022-06-1.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $fn = "./input-06.txt";
+open my $fh, '<', $fn or die $!;
+chomp( my @lines = <$fh> );
+close($fh);
+my %seen;
+my $seencount;
+foreach my $s (@lines) {
+    for my $i ( 0 .. length($s) - 4 ) {
+        my $s1 = substr( $s, $i, 4 );
+        %seen      = ();
+        $seencount = 0;
+        for my $j ( 0 .. length($s1) ) {
+            my $ch = substr( $s1, $j, 1 );
+            if ( $seen{$ch} ) {
+                $seen{$ch} += 1;
+            }
+            else {
+                $seen{$ch} += 1;
+                $seencount += 1;
+            }
+            if ( $seencount == 5 ) {
+                my $result = $i + 4;
+                print "Result = $result\n";
+                exit;
+            }
+        }
+    }
+}
+
