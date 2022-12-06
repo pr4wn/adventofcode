2ae499e06f9b2d31b46a4e1eebabbae3298a85ab
Seán Doherty <sean.prawn@gmail.com>
2022-12-06 11:13:26 +0000

    correct strlen to strlen - 1 because 0

--- 06/aoc-2022-06-1.pl
+++ 06/aoc-2022-06-1.pl
@@ -6,20 +6,18 @@ my $fn = "./input-06.txt";
 open my $fh, '<', $fn or die $!;
 chomp( my @lines = <$fh> );
 close($fh);
-my %seen;
-my $seencount;
 foreach my $s (@lines) {
     for my $i ( 0 .. length($s) - 4 ) {
-        my $s1 = substr( $s, $i, 4 );
-        %seen      = ();
-        $seencount = 0;
-        for my $j ( 0 .. length($s1) ) {
+        my $s1        = substr( $s, $i, 4 );
+        my %seen      = ();
+        my $seencount = 0;
+        for my $j ( 0 .. length($s1) - 1 ) {
             my $ch = substr( $s1, $j, 1 );
             if ( !$seen{$ch} ) {
                 $seen{$ch} += 1;
                 $seencount += 1;
             }
-            if ( $seencount == 5 ) {
+            if ( $seencount == 4 ) {
                 my $result = $i + 4;
                 print "Result = $result\n";
                 exit;
--- 06/aoc-2022-06-2.pl
+++ 06/aoc-2022-06-2.pl
@@ -6,20 +6,19 @@ my $fn = "./input-06.txt";
 open my $fh, '<', $fn or die $!;
 chomp( my @lines = <$fh> );
 close($fh);
-my %seen;
-my $seencount;
 foreach my $s (@lines) {
     for my $i ( 0 .. length($s) - 14 ) {
-        my $s1 = substr( $s, $i, 14 );
-        %seen      = ();
-        $seencount = 0;
-        for my $j ( 0 .. length($s1) ) {
+        my $s1        = substr( $s, $i, 14 );
+        my %seen      = ();
+        my $seencount = 0;
+        for my $j ( 0 .. length($s1) - 1 ) {
             my $ch = substr( $s1, $j, 1 );
             if ( !$seen{$ch} ) {
                 $seen{$ch} += 1;
                 $seencount += 1;
             }
-            if ( $seencount == 15 ) {
+
+            if ( $seencount == 14 ) {
                 my $result = $i + 14;
                 print "Result = $result\n";
                 exit;
